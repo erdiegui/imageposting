ac1eca8067e06af733b3c3e8f06b1460ad7e6b1a
dguzman <dguzman@paradigmatecnologico.com>
2016-08-12 15:38:26 +0200

    Working form with preview file

--- src/AppBundle/Form/ReplyFormType.php
+++ src/AppBundle/Form/ReplyFormType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -35,8 +36,10 @@ class ReplyFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class, array('label' => 'Title'))
-            ->add('imageFile', FilePreviewType::class, array('label' => 'New Image'));
+            ->add('title', TextType::class, array('label' => 'Title', 'required' => false))
+            ->add('imageFile', FilePreviewType::class, array('label' => 'New Image', 'required' => true))
+            ->add('submit', SubmitType::class, array('label' => 'Upload new image'))
+        ;
     }
 
     public function configureOptions(
